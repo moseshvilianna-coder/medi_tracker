0dc9f3dd494e19e9ace9ac081761f6cd5484ec6b
moseshvilianna-coder <moseshvilianna@gmail.com>
2026-03-10 23:37:28 +0400

    add session authentication for browser testing

--- core/settings.py
+++ core/settings.py
@@ -100,10 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
